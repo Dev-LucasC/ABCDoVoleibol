15eb432857b660c94382a29bf430c2af024aa69b
Dev-LucasC <lucaconceicao@hotmail.com>
2023-03-14 19:59:45 -0300

    Parceiros

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,39 @@
 import React from 'react'
 import './footer.css'
+import Fisio from './patrocinadores/2afisio.png'
+import Baggio from './patrocinadores/baggio.png'
+import Coc from './patrocinadores/coc.png'
+import Espeto from './patrocinadores/espetodeouro.png'
+import Giongo from './patrocinadores/giongo.png'
+import Hugo from './patrocinadores/hugo.png'
+import Macon from './patrocinadores/macon.png'
+import Panificadora from './patrocinadores/panificadoratiosilva.png'
+import Restaurante from './patrocinadores/restaurantedafamilia.png'
+import Visax from './patrocinadores/visax.png'
+import Yazigi from './patrocinadores/yazigi.png'
+
+
+
 
 
 const Footer = () => {
   return (
     <div className='parceiros'>
-      <p> ©Desenvolvido por  <a href="#"> Lucas da Conceição</a></p>
       <div>
         <h1>Parceiros</h1>
+        <div className='patrocinadores'>
+          <a href="#"><img src={Fisio}  alt="" className='imagem-grande' /></a>
+          <a href="#"><img src={Baggio} alt="" /></a>
+          <a href="#"><img src={Coc} alt="" /></a>
+          <a href="#"><img src={Espeto} alt="" /></a>
+          <a href="#"><img src={Giongo} alt="" /></a>
+          <a href="#"><img src={Hugo} alt="" /></a>
+          <a href="#"><img src={Macon} alt="" /></a>
+          <a href="#"><img src={Panificadora} alt="" /></a>
+          <a href="#"><img src={Restaurante} alt="" /></a>
+          <a href="#"><img src={Visax} alt="" /></a>
+          <a href="#"><img src={Yazigi} alt="" /></a>
+        </div>
       </div>
     </div>
   )
