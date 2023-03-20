a693b2cf513b5793ed9c6290394467e449aa363a
Dev-LucasC <lucaconceicao@hotmail.com>
2023-03-20 18:57:30 -0300

    Atualizacao cliente

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@
 import './App.css';
 import Header from './components/Header'
 import Teste from './components/Tela teste/Teste';
-import Galeria from './components/galeria/Galeria';
 import { Slider } from './components/noticias/slider';
 
 import Footer from './components/footer/Footer';
@@ -23,7 +22,6 @@ function App() {
     <Header />
     <Slider />
     <Outlet />
-    <Galeria />
     <Footer />
     </>  
   );
