1775fba33cfc6a807d60767c343dc297172650e9
Dev-LucasC <lucaconceicao@hotmail.com>
2023-04-20 19:50:53 -0300

    Arrumando api

--- src/routes/Equipes/Equipes.jsx
+++ src/routes/Equipes/Equipes.jsx
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import NavHeader from '../../components/NavHeader/NavHeader';
 import Header from '../../components/Header';
-import { PainelNoticias } from '../../components/Noticias/PainelNoticias';
 import Footer from '../../components/footer/Footer';
+import './equipes.css'
+
 export const Equipes = () => {
   const [posts, setPosts] = useState([]);
-  const [images, setImages] = useState({});
+
 
   useEffect(() => {
-    axios.get("https://hammerhead-app-5cwy4.ondigitalocean.app/api/calendarios")
+    axios.get("https://king-prawn-app-bnxyc.ondigitalocean.app/api/calendarios?populate=*")
       .then((response) => {
         const { data } = response.data;
         setPosts(data);
@@ -17,39 +18,7 @@ export const Equipes = () => {
       .catch((error) => {
         console.log(error);
       });
-
-    axios
-      .get(`https://hammerhead-app-5cwy4.ondigitalocean.app/api/upload/files`)
-      .then((response) => {
-        const { data } = response;
-        const filteredData1 = data.filter((file) => file.caption === "equipe1");
-        const filteredData2 = data.filter((file) => file.caption === "equipe2");
-
-        filteredData1.forEach((file) => {
-          const url = "https://hammerhead-app-5cwy4.ondigitalocean.app" + file.url;
-          console.log("url1:", url); // Verificar os valores de file.associatedId e url
-          setImages(prevState => ({
-            ...prevState,
-            [file.id]: url
-          }));
-        });
-
-
-        filteredData2.forEach((file) => {
-          const url = "https://hammerhead-app-5cwy4.ondigitalocean.app" + file.url;
-          setImages(prevState => ({
-            ...prevState,
-            [file.id]: url
-          }));
-        });
-
-      })
-
-      .catch((error) => {
-        console.log(error);
-      });
   }, []);
-
 
 
   return (
@@ -57,23 +26,25 @@ export const Equipes = () => {
       <NavHeader />
       <Header />
       {posts.map((post, index) => {
-       const image1Url = images[`${post.id}`];
-       const image2Url = images[`${post.id}`];
-        console.log("TESTE", image1Url)
-        return (
-          <div className='container_noticias' key={post.id}>
-            <h2>{post?.attributes?.local}</h2>
-            <p>{post?.attributes?.data}</p>
-            {Object.keys(images).includes(`${post.id}-equipe1`) && Object.keys(images).includes(`${post.id}-equipe2`) && (
-              <div className="container_imagens">
-                <img src={image1Url} alt="" />
-                <img src={image2Url} alt="" />
-              </div>
-            )}
 
-          </div>
+        console.log("teste", post)
+        return (
+          <section>
+            <div className='container_confronto'>
+              <div className='confronto'>
+                <img src={"https://king-prawn-app-bnxyc.ondigitalocean.app" + post?.attributes.time1.data[0].attributes.formats.thumbnail.url} alt="" />
+                <h1>X</h1>
+                <img src={"https://king-prawn-app-bnxyc.ondigitalocean.app" + post?.attributes.time2.data[0].attributes.formats.thumbnail.url} alt="" />
+              </div>
+              <h2>Local: {post?.attributes?.local}</h2>
+              <p>Data: {post?.attributes?.data}</p>
+              <div className='portofolio__item-cta'>
+              </div>
+            </div>
+          </section>
         );
       })}
+
       <Footer />
 
     </>
