7ea777aabad6e07aaf9ecca3f8aac6b31237c15c
Lucas Conceição <lucasconceicao@MacBook-Air-de-Lucas.local>
2023-04-21 18:54:21 -0300

    Calendario de confrontos

--- src/components/Inicio/slider.jsx
+++ src/components/Inicio/slider.jsx
@@ -3,16 +3,13 @@ import { Navigation, Pagination, A11y, EffectCube } from 'swiper';
 import 'swiper/swiper-bundle.min.css';
 import './App.css'
 import { Link } from 'react-router-dom';
-
-
-
+import { Confrontos } from '../../routes/Calendario/Confrontos'
 import React, { useState, useEffect } from "react";
 import axios from 'axios'
 
 export const Slider = ({ slides }) => {
 
   const [posts, setPosts] = useState([]);
-
 
   useEffect(() => {
     axios.get("https://king-prawn-app-bnxyc.ondigitalocean.app/api/noticias?populate=*")
@@ -25,42 +22,41 @@ export const Slider = ({ slides }) => {
       });
   }, []);
 
-
   return (
-    <section>
-      <div>
+    <section className='container'>
+      <div className='noticia'>
         <h1> Noticias </h1>
         <hr size="6" width="100%" align="left" color="black"></hr>
+        <Swiper
+          modules={[Navigation, Pagination, A11y, EffectCube]}
+          spaceBetween={20}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true, bottom: 100 }}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          {posts.map((post, index) => {
+            return (
+              <SwiperSlide key={index}>
+                <div className='container_noticias'>
+                  <div className='portofolio__item-img'>
+                    <img src={"https://king-prawn-app-bnxyc.ondigitalocean.app" + post?.attributes.imagem.data[0].attributes.url} alt={post?.attributes?.titulo} />
+                  </div>
+                  <h2>{post?.attributes?.titulo}</h2>
+                  <p>{post?.attributes?.texto}</p>
+                  <div className='portofolio__item-cta'>
+                    <Link to={`/noticias/${post?.id}`} className='btn'>Ver noticia</Link>
+                  </div>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
-      <Swiper
-        modules={[Navigation, Pagination, A11y, EffectCube]}
-        spaceBetween={20}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true, bottom: 100 }}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
-
-      >{posts.map((post, index) => {
-
-        return (
-          <SwiperSlide key={index}>
-            <div className='container_noticias'>
-              <div className='portofolio__item-img'>
-                <img src={"https://king-prawn-app-bnxyc.ondigitalocean.app" + post?.attributes.imagem.data[0].attributes.url} alt={post?.attributes?.titulo} />
-              </div>
-              <h2>{post?.attributes?.titulo}</h2>
-              <p>{post?.attributes?.texto}</p>
-              <div className='portofolio__item-cta'>
-                <Link to={`/noticias/${post?.id}`} className='btn'>Ver noticia</Link>
-              </div>
-            </div>
-          </SwiperSlide>
-        );
-
-      })}
-      </Swiper>
+      <div className='confrontos'>
+        <Confrontos />
+      </div>
     </section>
   )
-
 }
--- src/routes/Calendario/Calendario.jsx
+++ src/routes/Calendario/Calendario.jsx
@@ -3,48 +3,16 @@ import axios from 'axios';
 import NavHeader from '../../components/NavHeader/NavHeader';
 import Header from '../../components/Header';
 import Footer from '../../components/footer/Footer';
+import { Confrontos } from './Confrontos';
 import './calendario.css'
 
 export const Calendario = () => {
-  const [posts, setPosts] = useState([]);
-
-
-  useEffect(() => {
-    axios.get("https://king-prawn-app-bnxyc.ondigitalocean.app/api/calendarios?populate=*")
-      .then((response) => {
-        const { data } = response.data;
-        setPosts(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
 
   return (
     <>
       <NavHeader />
       <Header />
-      <div className='container_posts'>
-        {posts.map((post, index) => {
-          <h1> PROXIMOS JOGOS </h1>
-          return (
-            <div className='container_calendario'>
-              <div className='container_confronto'>
-                <h2>{post?.attributes?.categoria}</h2>
-                <div className='confronto'>
-                  <img src={"https://king-prawn-app-bnxyc.ondigitalocean.app" + post?.attributes.time1.data[0].attributes.formats.thumbnail.url} alt="" />
-                  <h1>X</h1>
-                  <img src={"https://king-prawn-app-bnxyc.ondigitalocean.app" + post?.attributes.time2.data[0].attributes.formats.thumbnail.url} alt="" />
-                  </div>
-                <h2>Local: {post?.attributes?.local}</h2>
-                <p>Data: {post?.attributes?.data}</p>
-               
-               
-              </div>
-            </div>
-          );
-        })}
-      </div>
+     <Confrontos />
       <Footer />
     </>
   );
