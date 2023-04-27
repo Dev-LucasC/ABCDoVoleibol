bc9a2be1df20e019b224fd7bb1965a3c7f5713a1
Dev-LucasC <lucaconceicao@hotmail.com>
2023-04-27 20:48:03 -0300

    Logo arrumada e containers ajustados

--- src/components/NavHeader/NavHeader.jsx
+++ src/components/NavHeader/NavHeader.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Logo from './logo.png'
 import './navheader.css'
-import { FaLinkedin } from 'react-icons/fa'
 import { FaFacebookSquare } from 'react-icons/fa'
 import { FaInstagram } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
@@ -12,29 +11,28 @@ const Teste = () => {
   return (
     <header>
       <div className="container-header">
-      <img src={Logo} alt="" />
+        <img src={Logo} alt="" />
         <div className="header-sociais">
-          <a href="" color="#343746"><FaLinkedin /> </a>
-          <a href="#"><FaFacebookSquare /> </a>
-          <a href="#"><FaInstagram /> </a>
+          <a href="https://www.facebook.com/abcdovoleibol?mibextid=ZbWKwL" target='_blank'><FaFacebookSquare /> </a>
+          <a href="https://instagram.com/abcdovoleibol?igshid=ZDdkNTZiNTM=" target='_blank'><FaInstagram /> </a>
         </div>
       </div>
       <nav>
         <ul>
           <li>
-           <Link to="/"> Inicio </Link>
+            <Link to="/"> Inicio </Link>
           </li>
           <li>
             <Link to="/contas">  Prestação de Contas</Link>
           </li>
           <li>
-          <Link to="/noticias"> Noticias </Link>
+            <Link to="/noticias"> Noticias </Link>
           </li>
           <li>
-          <Link to="/equipes"> Equipes</Link>
+            <Link to="/equipes"> Equipes</Link>
           </li>
           <li>
-          <Link to="/calendario"> Calendario</Link>
+            <Link to="/calendario"> Calendario</Link>
           </li>
         </ul>
       </nav>
