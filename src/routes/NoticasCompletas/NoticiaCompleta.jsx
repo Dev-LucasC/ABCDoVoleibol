f1ea524d79f812588dfbea4ca023a80039f404d9
Dev-LucasC <lucaconceicao@hotmail.com>
2023-05-02 10:14:10 -0300

    Noticias Por Id arrumadas

--- src/routes/NoticasCompletas/NoticiaCompleta.jsx
+++ src/routes/NoticasCompletas/NoticiaCompleta.jsx
@@ -1,47 +1,44 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import './noticiacompleta.css'
 import Header from '../../components/Header/index'
 import NavHeader from '../../components/NavHeader/NavHeader';
 
 const NoticiaCompleta = () => {
-
-  const [posts, setPosts] = useState([]);
-
+  const { id } = useParams();
+  const [post, setPost] = useState(null);
 
   useEffect(() => {
-    axios.get("https://king-prawn-app-bnxyc.ondigitalocean.app/api/noticias?populate=*")
+    axios.get(`https://king-prawn-app-bnxyc.ondigitalocean.app/api/noticias/${id}?populate=*`)
       .then((response) => {
         const { data } = response.data;
-        setPosts(data);
+        setPost(data);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [id]);
 
-
+  if (!post) {
+    return <p>Carregando...</p>;
+  }
 
   return (
     <>
       <NavHeader />
       <Header />
-      {posts.map((post, index) => {
-        return (
-            <div className='container_noticias'>
-              <div className='portofolio__item-img'>
-                <img src={"https://king-prawn-app-bnxyc.ondigitalocean.app" + post?.attributes.imagem.data[0].attributes.url} alt={post?.attributes?.titulo} />
-              </div>
-              <h2>{post?.attributes?.titulo}</h2>
-              <p>{post?.attributes?.texto}</p>
-              <div className='portofolio__item-cta'>
-                <Link to={`/noticias`} className='btn'>Voltar</Link>
-              </div>
-            </div>
-        );
-      })}
+      <div className='container_noticias'>
+        <div className='portofolio__item-img'>
+          <img src={"https://king-prawn-app-bnxyc.ondigitalocean.app" + post?.attributes.imagem.data[0].attributes.url} alt={post?.attributes?.titulo} />
+        </div>
+        <h2>{post?.attributes?.titulo}</h2>
+
+        <p>{post?.attributes?.texto}</p>
+        <div className='portofolio__item-cta'>
+          <Link to={`/noticias`} className='btn'>Voltar</Link>
+        </div>
+      </div>
     </>
   );
 };
