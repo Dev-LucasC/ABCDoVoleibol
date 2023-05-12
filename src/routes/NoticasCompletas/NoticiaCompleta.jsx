c808d4b9fde68a6db6199813dff0dc5fd017379e
Dev-LucasC <lucaconceicao@hotmail.com>
2023-05-12 14:17:48 -0300

    Trabalhando estilos

--- src/routes/NoticasCompletas/NoticiaCompleta.jsx
+++ src/routes/NoticasCompletas/NoticiaCompleta.jsx
@@ -8,12 +8,24 @@ import NavHeader from '../../components/NavHeader/NavHeader';
 const NoticiaCompleta = () => {
   const { id } = useParams();
   const [post, setPost] = useState(null);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     axios.get(`https://shark-app-6myi8.ondigitalocean.app/api/noticias/${id}?populate=*`)
       .then((response) => {
         const { data } = response.data;
         setPost(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    axios.get("https://shark-app-6myi8.ondigitalocean.app/api/noticias?populate=*")
+      .then((response) => {
+        const { data } = response.data;
+        const sortedData = data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+        const filteredData = sortedData.slice(0, 9);
+        setPosts(filteredData);
       })
       .catch((error) => {
         console.log(error);
@@ -28,14 +40,37 @@ const NoticiaCompleta = () => {
     <>
       <NavHeader />
       <Header />
-      <div className='container-noticias_completa'>
-        <div className='container-noticiascompleta_img'>
-         <img src={post?.attributes.imagem.data.attributes.url} alt={post?.attributes?.titulo} loading="lazy" />
+      <div className="news-container">
+        <div className="news-details">
+          <h2>{post?.attributes?.titulo}</h2>
+          <div className="news-image">
+            <img src={post?.attributes.imagem.data.attributes.url} alt={post?.attributes?.titulo} loading="lazy" />
+          </div>
+
+          <p>{post?.attributes?.texto}</p>
+          <div className="news-link-container">
+            <Link to={`/noticias`} className="news-link">
+              Voltar
+            </Link>
+          </div>
         </div>
-        <h2>{post?.attributes?.titulo}</h2>
-        <p>{post?.attributes?.texto}</p>
-        <div className='portofolio__item-cta-completa'>
-          <Link to={`/noticias`} className='btn'>Voltar</Link>
+        <div className="other-news">
+          {posts.map((item) => (
+            <div key={item.id} className="news-item">
+              <div className="news-item-image">
+                <img src={item?.attributes.imagem.data.attributes.url} alt={item?.attributes?.titulo} loading="lazy" />
+              </div>
+              <div className="news-item-details">
+                <h3 className="news-item-title">{item?.attributes?.titulo}</h3>
+                <p className="news-item-text">{item?.attributes?.texto}</p>
+                <div className="news-item-link-container">
+                  <Link to={`/noticias/${item?.id}`} className="news-item-link">
+                    Ver notícia completa
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
