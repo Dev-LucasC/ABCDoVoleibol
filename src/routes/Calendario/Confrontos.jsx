f850c8ab16fb3b9684d1df27706988d3c086bf2c
Dev-LucasC <lucaconceicao@hotmail.com>
2023-06-06 21:37:40 -0300

    Filtros e categorias

--- src/routes/Calendario/Confrontos.jsx
+++ src/routes/Calendario/Confrontos.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import moment from 'moment';
 import 'moment/locale/pt-br';
-
+import { Link } from 'react-router-dom';
 import './calendario.css';
 
 export const Confrontos = () => {
@@ -13,8 +13,8 @@ export const Confrontos = () => {
       .get("https://shark-app-6myi8.ondigitalocean.app/api/calendarios?populate=*")
       .then((response) => {
         const { data } = response.data;
-        const confrontosFuturos = data.filter((confronto) => moment(confronto.attributes.data).isAfter(moment()));
-        confrontosFuturos.sort((a, b) => moment(a.attributes.data).diff(moment(b.attributes.data)));
+        const confrontosFuturos = data.filter((confronto) => moment(confronto.attributes.data).isSameOrBefore(moment().add(1, 'day'), 'day'));
+        confrontosFuturos.sort((a, b) => moment(a.attributes.data).diff(moment(b.attributes.data), 'days'));
         setConfrontosOrganizados(confrontosFuturos);
       })
       .catch((error) => {
@@ -28,7 +28,7 @@ export const Confrontos = () => {
         return (
           <div key={index} className='container_calendario'>
             <div className='container_confronto'>
-              <h2>{confronto?.attributes?.categoria}</h2>
+              <h2>Categoria: {confronto?.attributes?.categoria}</h2>
               <div className='confronto'>
                 <img src={confronto?.attributes.time2.data[0].attributes.url} loading="lazy" alt="Time 2" />
                 <h1>X</h1>
@@ -36,6 +36,9 @@ export const Confrontos = () => {
               </div>
               <h2>Local: {confronto?.attributes?.local}</h2>
               <p>Data: {moment(confronto?.attributes?.data).format("DD/MM/YYYY")}</p>
+              <div className='btn-container'>
+                <Link to={`/jogoaovivo`} className='btn'>Ver Jogo</Link>
+              </div>
             </div>
           </div>
         );
